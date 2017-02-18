6b901b797a81cb8609c0e6444cd9bcb13a03dcba
travelhacks <apoinasdfx@bredband.net>
2017-02-18 13:05:14 +0100

    Add options parser

---
+++ FlysasClient/Options.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlysasClient
+{
+    public class Options
+    {
+        public bool OutputBookingClass { get; private set; }
+
+        public bool Parse(string s)
+        {
+            var stack = new Stack<string>(s.ToLower().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Reverse());
+            if (stack.Any() && stack.Pop() == "set")
+            {
+                while (stack.Count >= 2)
+                {
+                    var option = stack.Pop();
+                    var sVal = stack.Pop();
+                    switch (option)
+                    {
+                        case "bookingclass": OutputBookingClass = myBool(sVal); break;
+                    }
+                }
+                return true;
+            }
+            else return false;
+        }
+        public bool myBool(string s)
+        {
+            return s == "on" || s == "true" || s == "1" || s == "yes";
+        }
+
+    }
+}
