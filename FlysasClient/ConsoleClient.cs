2ad6b17eff40c1b7ef19ac16a33657bd29b497f0
travelhacks <apoinasdfx@bredband.net>
2017-02-18 13:11:56 +0100

    Handle booking class option

--- FlysasClient/ConsoleClient.cs
+++ FlysasClient/ConsoleClient.cs
@@ -11,35 +11,51 @@ namespace FlysasClient
             var client = new SASRestClient();
             string input = null;
             var parser = new Parser();
+            var options = new FlysasClient.Options();
+
+
             while (input != "q")
             {
                 Console.WriteLine("Syntax: Origin-Destination outDate [inDate]");
                 Console.Write(">>");
                 input = Console.ReadLine();
-                SASQuery req = null;
-                try
-                {
-                    req = parser.Parse(input);                    
-                }catch(Exception ex)
-                {
-                    Console.Write("Error parsing input. ");
-                }
-                if (req != null)
-                {
-                    var res = client.Search(req);
-                    Console.WriteLine("*********Outbound*******");
-                    PrintFlights(res.outboundFlights, res.outboundFlightProducts);
-                    if (req.InDate.HasValue)
+                if (!options.Parse(input))
+                    foreach (string query in input.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
                     {
-                        Console.WriteLine("*********Inbound*******");
-                        PrintFlights(res.inboundFlights, res.inboundFlightProducts);
+                        SASQuery req = null;
+                        try
+                        {
+                            req = parser.Parse(query);
+                        }
+                        catch (ParserException ex)
+                        {
+                            Console.Write("Syntax error:" + ex.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Write("Syntax error:");
+                        }
+                        if (req != null)
+                        {
+                            var res = client.Search(req);
+                            Console.WriteLine("*********Outbound*******");
+                            PrintFlights(res.outboundFlights, res.outboundFlightProducts, options);
+                            if (req.InDate.HasValue)
+                            {
+                                Console.WriteLine("*********Inbound*******");
+                                PrintFlights(res.inboundFlights, res.inboundFlightProducts, options);
+                            }
+                        }
+                        Console.Write(Environment.NewLine + Environment.NewLine);
                     }
-                }
-                Console.Write(Environment.NewLine + Environment.NewLine);
             }
+
         }
-        void PrintFlights(IEnumerable<FlightBaseClass> flights, IEnumerable<FlightProductBaseClass> products)
+        void PrintFlights(IEnumerable<FlightBaseClass> flights, IEnumerable<FlightProductBaseClass> products, FlysasClient.Options options)
         {
+            bool printClasses = options.OutputBookingClass;
+            string tab = "\t";
+            int tabLen = 8;
             if (flights == null)
             {
                 Console.WriteLine("No flights found");
@@ -47,16 +63,36 @@ namespace FlysasClient
             }
             string format = "HH:mm";
             var codes = products.Select(p => p.productCode).Distinct().ToArray();
-            var header = "Dep\tArr\t" + string.Join("\t", codes);
+            var first = flights.First();
+            var header = $"{first.origin.code}{tab}{first.destination.code}{tab}" + string.Join(tab + (printClasses ? tab : ""), codes);
             Console.WriteLine(header);
             foreach (var r in flights)
             {
                 var prices = products.Where(p => p.id.EndsWith("_" + r.id.ToString())).ToArray();
-                Console.Write($"{r.startTimeInLocal.ToString(format)}\t{r.endTimeInLocal.ToString(format)}");
+                var dateDiff = (r.endTimeInLocal.Date - r.startTimeInGmt.Date).Days;
+                Console.Write($"{r.startTimeInLocal.ToString(format)}{tab}{r.endTimeInLocal.ToString(format)}" + (dateDiff > 0 ? "+" + dateDiff : ""));
                 foreach (var c in codes)
                 {
+                    string sClasses = "";
                     var p = prices.FirstOrDefault(price => price.productCode == c);
-                    Console.Write("\t" + (p != null ? p.price.formattedTotalPrice : ""));
+                    var sPrice = "";
+                    if (p != null)
+                    {
+
+                        var classes = p.fares.Select(f => f.bookingClass + f.avlSeats);
+                        if (classes.Distinct().Count() == 1)
+                            sClasses = classes.First();
+                        else
+                        {
+                            sClasses = string.Join("/", classes);
+                            if (sClasses.Length >= tabLen)
+                                sClasses = string.Join("/", p.fares.Select(f => f.bookingClass));
+                        }
+                        sPrice = p.price.formattedTotalPrice;
+                    }
+                    Console.Write(tab + sPrice);
+                    if (printClasses)
+                        Console.Write(tab + sClasses);
                 }
                 Console.Write(Environment.NewLine);
             }
