aa5b2237c35f9cb3346c5a0ccdb0617732ee3ac4
travelhacks <apoinasdfx@bredband.net>
2017-02-21 21:08:44 +0100

    Refactor download

--- FlysasLib/SASRestClient.cs
+++ FlysasLib/SASRestClient.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
-
 
 namespace FlysasLib
 {
@@ -19,28 +17,22 @@ namespace FlysasLib
                 {
                     lock (padLock)
                     {
-                        var req = new HttpRequestMessage()
+                        var request = new HttpRequestMessage()
                         {
                             RequestUri = new Uri("https://www.sas.se/bin/sas/d360/getOauthToken"),
-                            Method = HttpMethod.Post                            
+                            Method = HttpMethod.Post
                         };
-                        var task = client.SendAsync(req).ContinueWith(t =>
-                        {
-                            var stringTask = t.Result.Content.ReadAsStringAsync();
-                            stringTask.Wait();
-                            var auth = Newtonsoft.Json.JsonConvert.DeserializeObject<AuthResponse>(stringTask.Result);                            
-                            accessToken = auth.access_token;
-                        });
-                        task.Wait();
+                        var jSon = downLoad(request);
+                        var auth = Newtonsoft.Json.JsonConvert.DeserializeObject<AuthResponse>(jSon);
+                        accessToken = auth.access_token;
                     }
-                }                
+                }            
                 return accessToken;
             }
         }
 
         public SearchResult Search(SASQuery query)
-        {
-            
+        {            
             try
             {
               return search(query);
@@ -51,9 +43,9 @@ namespace FlysasLib
                 return search(query);
             }
         }
+      
         private SearchResult search(SASQuery query)
-        {
-            SearchResult root = null;
+        {            
             var req = new HttpRequestMessage(){ RequestUri = query.GetUrl()};                        
             req.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(AccessToken);
             req.Headers.AcceptEncoding.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("gzip"));
@@ -61,18 +53,24 @@ namespace FlysasLib
             req.Headers.CacheControl = new System.Net.Http.Headers.CacheControlHeaderValue() { NoCache = true };
             req.Headers.Pragma.Add(new System.Net.Http.Headers.NameValueHeaderValue("no-cache"));
             req.Headers.Connection.Add("keep-alive");
+            var jSon = downLoad(req);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<SearchResult>(jSon);            
+        }
 
-            var task = client.SendAsync(req).ContinueWith(t =>
+        private string downLoad(HttpRequestMessage request)
+        {
+            string res = null;
+            var task = client.SendAsync(request).ContinueWith(t =>
             {
                 HttpResponseMessage response = t.Result;
                 if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
                     throw new Exception("Bad request");
-                var stringTask = response.Content.ReadAsStringAsync();                
-                stringTask.Wait();
-                root = Newtonsoft.Json.JsonConvert.DeserializeObject<SearchResult>(stringTask.Result);
+                var readTask = response.Content.ReadAsStringAsync();
+                readTask.Wait();
+                res = readTask.Result;
             });
-            task.Wait();            
-            return root;
+            task.Wait();
+            return res;
         }
     }
 }
