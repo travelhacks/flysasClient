afa2643ce9c481110fc17672014c63312ad93a5b
travelhacks <apoinasdfx@bredband.net>
2017-03-27 21:40:54 +0200

    Add openflights container class

---
+++ OpenFlightsData/OFData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenFlightsData
+{
+    public class OFData
+    {
+        public IReadOnlyList<Airline> Airlines { get; private set; } = new List<Airline>();
+        public IReadOnlyList<Airport> Airports { get; private set; } = new List<Airport>();
+        public IReadOnlyList<Route> Routes { get; private set; } = new List<Route>();
+
+        public void LoadData()
+        {
+            Airlines = new Airline().GetAll();
+            Airports =  new Airport().GetAll();
+            Routes = new Route().GetAll();
+        }
+    }
+    
+}
