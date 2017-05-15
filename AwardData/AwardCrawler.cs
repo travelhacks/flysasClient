1fd395eae34dc459a25a2295733292303583c3e8
travelhacks <apoinasdfx@bredband.net>
2017-05-15 19:55:56 +0200

    Award data changes

--- AwardData/AwardCrawler.cs
+++ AwardData/AwardCrawler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 
 
@@ -12,68 +13,78 @@ namespace AwardData
             ctx = context;
         }
 
-        public void Crawl(bool checkOldEmpty,bool checkExisting, int MaxAgeInDays,int sleepSeconds)
+        public void Crawl(int start,int stop,bool force,bool checkOldEmpty,bool checkExisting, int MaxAgeInDays,int sleepSeconds)
         {
             var sasClient = new FlysasLib.SASRestClient();
-            var start = DateTime.Now.Date;            
-            
-            int startDay = checkExisting || checkOldEmpty ? 1 : 325;
-            int endDay = 330;
-            
+                               
             foreach(var route in ctx.Routes.ToList())
-            foreach(var returnFlight in new [] {false,true })
-            for (int i = startDay; i <= endDay; i++)
-            {
-                var q = new FlysasLib.SASQuery
+            {                
+                foreach (var returnFlight in new[] { false, true })
                 {
-                    OutDate = start.AddDays(i),
-                    From = returnFlight ? route.To : route.From,
-                    To = returnFlight ? route.From : route.To,
-                };
-                try
-                {
-                            var old = ctx.Crawls.Where(crawl => crawl.RouteId == route.Id && crawl.Return == returnFlight && crawl.TravelDate == q.OutDate.Value && crawl.Success).OrderBy(f=>f.CrawlDate).FirstOrDefault();
+                    var oldFlights = ctx.Crawls.FromSql("select * from v_latestcrawls").Where(crawl => crawl.RouteId == route.Id && crawl.Return == returnFlight && crawl.Success).ToList();
+                    for (int i = start; i <= stop; i++)
+                    {
+                        var q = new FlysasLib.SASQuery
+                        {
+                            OutDate = DateTime.Now.Date.AddDays(i),
+                            From = returnFlight ? route.To : route.From,
+                            To = returnFlight ? route.From : route.To,
+                        };
+                        try
+                        {                            
+                            var old = oldFlights.Where(crawl => crawl.RouteId == route.Id && crawl.Return == returnFlight && crawl.TravelDate == q.OutDate.Value && crawl.Success).OrderByDescending(f => f.CrawlDate).FirstOrDefault();
                             if (old != null)
                             {
-                                if (i < 325 && (!checkExisting && old.Business > 0|| old.Business == 0 && (!checkOldEmpty  || old.CrawlDate > DateTime.UtcNow.AddDays(-MaxAgeInDays))))
+                                if (!force && (!checkExisting && old.Business > 0 || old.Business == 0 && (!checkOldEmpty || old.CrawlDate > DateTime.UtcNow.AddDays(-MaxAgeInDays))))
                                     continue;
                             }
 
-                    FlysasLib.AwardResult res = null;
-                    var availability = new int[3];
-                    do
-                    {
-                        res = sasClient.Award(q);
-                        System.Threading.Thread.Sleep(1000*sleepSeconds);
-                        if (res != null)
-                        {
-                            if (res != null && res.outbounds != null)
+                            FlysasLib.AwardResult res = null;
+                            var availability = new int[3];
+                            FlysasLib.AwardFlight flight = null;
+                            do
                             {
-                                foreach (var f in res.outbounds.Where(f => f.segments.Count == 1))
+                                res = sasClient.Award(q);
+                                System.Threading.Thread.Sleep(1000 * sleepSeconds);
+                                if (res != null)
                                 {
-                                    if (f.products.BUSINESS != null)
-                                        availability[2] = q.Adt;
-                                    if (f.products.PLUS != null)
-                                        availability[1] = q.Adt;
-                                    if (f.products.GO != null)
-                                        availability[0] = q.Adt;
-                                }
-                            }
-                        }
-                        q.Adt++;
-                    } while (res != null && res.httpSuccess && res.outbounds.Any(f => f.products != null && (f.products.BUSINESS != null)));
-                    
-                    var c = new AwardData.Crawl { Business = availability[2], Plus = availability[1], Go = availability[0], CrawlDate = DateTime.UtcNow, Return = returnFlight, RouteId = route.Id, TravelDate = q.OutDate.Value.Date };
-                    c.Success = res != null && (res.errors == null || res.errors.First().errorCode == "225036");
-                    ctx.Crawls.Add(c);
-                    ctx.SaveChanges();
-                    if (availability.Any(j => j > 0))
-                        Console.WriteLine(route.From + "-" + route.To + " "+ q.OutDate.Value.ToString("yyyyMMdd") + ":" +  string.Join(",", availability));
-                }
+                                    if (res != null && res.outbounds != null)
+                                    {
+                                        foreach (var f in res.outbounds.Where(f => f.segments.Count == 1))
+                                        {
+                                            if (f.products.BUSINESS != null)
+                                                availability[2] = q.Adt;
+                                            if (f.products.PLUS != null)
+                                                availability[1] = q.Adt;
+                                            if (f.products.GO != null)
+                                                availability[0] = q.Adt;
+                                            flight = f;
+                                        }
 
-                catch(Exception ex)
-                {
-                    Console.Write("Error for " + q.OutDate.ToString());
+                                    }
+                                }
+                                q.Adt++;
+                            } while (res != null && res.httpSuccess && res.outbounds.Any(f => f.products != null && (f.products.BUSINESS != null)));
+
+                            var c = new AwardData.Crawl { Business = availability[2], Plus = availability[1], Go = availability[0], CrawlDate = DateTime.UtcNow, Return = returnFlight, RouteId = route.Id, TravelDate = q.OutDate.Value.Date };
+                            if (flight != null)
+                            {
+                                c.Departure = flight.segments[0].startTimeLocal;
+                                c.Arrival = flight.segments[0].endTimeInLocal;
+                                c.Flight = flight.segments[0].flightNumber;
+                            }
+                            c.Success = res != null && (res.errors == null || res.errors.First().errorCode == "225036");
+                            ctx.Crawls.Add(c);
+                            ctx.SaveChanges();
+                            if (availability.Any(j => j > 0))
+                                Console.WriteLine(route.From + "-" + route.To + " " + q.OutDate.Value.ToString("yyyyMMdd") + ":" + string.Join(",", availability));
+                        }
+
+                        catch (Exception ex)
+                        {
+                            Console.Write("Error for " + q.OutDate.ToString());
+                        }
+                    }
                 }
             }
             Console.ReadKey();
--- AwardData/AwardData.cs
+++ AwardData/AwardData.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AwardData
 {
@@ -13,9 +14,15 @@ namespace AwardData
 
         public string ToString(bool Return)
         {
-            if (Return)
-                return $"{To}-{From}";
-            return $"{From}-{To}";
+            return GetFrom(Return) + "-" + GetTo(Return);                
+        }
+        public string GetTo(bool Return)
+        {
+            return Return ? From : To;
+        }
+        public string GetFrom(bool Return)
+        {
+            return Return ? To : From;
         }
     }
 
@@ -26,6 +33,9 @@ namespace AwardData
         public bool Return { get; set; }
         public DateTime TravelDate { get; set; }
         public DateTime CrawlDate { get; set; }
+        public DateTimeOffset? Arrival { get; set; }
+        public DateTimeOffset? Departure { get; set; }
+        public string Flight { get; set; }
         public virtual Route Route { get; set; }
         public int Go { get; set; }
         public int Plus { get; set; }
@@ -33,12 +43,45 @@ namespace AwardData
         public bool Success { get; set; }
     }
 
+    public class AwardExport
+    {
+        public int Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTimeOffset Arrival { get; set; }
+        public DateTimeOffset Departure { get; set; }
+        public int Business;
+        public int Plus;
+        public int Go;
+    }
+
+    
+
+    [Table("V_Changes")]
+    public class Changes
+    {
+        public int Id { get; set; }
+        public int RouteId { get; set; }
+        public bool Return { get; set; }
+        public DateTime TravelDate { get; set; }
+        public DateTime CrawlDate { get; set; }
+        public virtual Route Route { get; set; }        
+        public int Business { get; set; }        
+        public int? c2 { get; set; }
+    }
+
     public class AwardContext : DbContext
     {
         public AwardContext(DbContextOptions options) : base(options)
         {           
         }
+        public AwardContext(string connection) : base(new DbContextOptionsBuilder<AwardContext>().UseSqlServer(connection).Options)
+        {
+                    
+        }
         public DbSet<Route> Routes { get; set; }
         public DbSet<Crawl> Crawls { get; set; }
+        public DbSet<Changes> Changes { get; set; }
     }
 }
