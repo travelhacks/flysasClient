8d1c93b1c120220bd02b6a03017cb2e6090f9628
travelhacks <apoinasdfx@bredband.net>
2019-10-06 18:00:11 +0200

    ExtensionMethods.MyTrim

--- FlysasLib/ExtensionMethods.cs
+++ FlysasLib/ExtensionMethods.cs
@@ -12,13 +12,15 @@ namespace FlysasLib
 
         public static int MyLength(this string str) => str == null ? 0 : str.Length;
 
+        public static string MyTrim(this string str) => str?.Trim();
+
         public static string camelCase(this string str)
         {
             if (str.IsNullOrEmpty())
                 return string.Empty;
             return str.First().ToString().ToLower() + str.Substring(1);
         }
-        
+
         static IEnumerable<string> simplify(IEnumerable<string> list)
         {
             if (list.Distinct().Count() == 1)
@@ -30,6 +32,13 @@ namespace FlysasLib
         public static String SimplifyAndJoin(this IEnumerable<string> list, string separator)
         {
             return string.Join(separator, simplify(list));
+        }
+
+        public static int MyDayOfWeek(this DateTime dt)
+        {
+            if (dt.DayOfWeek == DayOfWeek.Sunday)
+                return 7;
+            return (int)dt.DayOfWeek;
         }
     }
 }
