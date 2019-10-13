adbdc64edaaeb1e5329015808a076e5cbbbd0f20
travelhacks <apoinasdfx@bredband.net>
2019-10-13 20:31:00 +0200

    Add UrlRewriter

---
+++ AwardWeb/Code/RedirectToHttpsNoWWW.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwardWeb.Code
+{
+    public class RedirectToHttpsNoWWW : IRule
+    {
+        public virtual void ApplyRule(RewriteContext context)
+        {
+            var req = context.HttpContext.Request;
+            var host = req.Host.Host;
+            if (!host.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                var isWWW = host.StartsWith("www.", StringComparison.OrdinalIgnoreCase);
+                if (isWWW || !req.IsHttps)
+                {
+                    if (isWWW)
+                        host = host.Substring(4);
+                    var url = UriHelper.BuildAbsolute("https", new Microsoft.AspNetCore.Http.HostString(host), req.PathBase, req.Path, req.QueryString);
+                    var response = context.HttpContext.Response;
+                    response.StatusCode = 301;
+                    response.Headers[HeaderNames.Location] = url;
+                    context.Result = RuleResult.EndResponse;
+                    return;
+                }
+            }
+            context.Result = RuleResult.ContinueRules;
+            return;
+        }    
+    }
+}
--- AwardWeb/Startup.cs
+++ AwardWeb/Startup.cs
@@ -85,9 +85,12 @@ namespace AwardWeb
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts();               
             }
-            
+            var redirOptions = new Microsoft.AspNetCore.Rewrite.RewriteOptions();
+            redirOptions.Rules.Add(new Code.RedirectToHttpsNoWWW());
+            app.UseRewriter(redirOptions);
+
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthentication();
