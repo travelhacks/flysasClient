76a1888714ad951836a3eafe6aad90c864dccd87
travelhacks <apoinasdfx@bredband.net>
2019-10-13 10:11:04 +0200

    Merge Alert-feature

---
+++ AwardWeb/Models/AlertMailModel.cs
@@ -0,0 +1,16 @@
+﻿using AwardData;
+using AwardWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwardWeb.Models
+{
+    public class AlertMailModel
+    {        
+        public ApplicationUser User { get; set; }            
+        public Dictionary<int, int> CountHash { get; internal set; }
+        public List<MailContainer> Rows { get; set; }
+    }
+}
