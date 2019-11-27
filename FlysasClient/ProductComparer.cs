18242e6ae6419c11bcff825572132717fd6b40c8
travelhacks <apoinasdfx@bredband.net>
2019-11-27 18:14:06 +0100

    Comparer, add new product codes

--- FlysasClient/ProductComparer.cs
+++ FlysasClient/ProductComparer.cs
@@ -8,9 +8,9 @@ namespace FlysasClient
     public class ProductComparer : IComparer<FlightProductBaseClass>
     {
         static List<string> order = new List<string>(
-          new[]   { "ECICNBG", "ECOD","ECOB","ECOA","ECOQ", "EBSARGO","ECONOMY",
-                        "PREMD", "PREMM", "PREML","EBSARP"
-                        ,"BUSB", "BUSA", "EBSARB","BUSINESS" }
+          new[]   { "SMECOD","ECICNBG", "ECOD","ECOB","ECOA","ECOQ", "EBSARGO","ECONOMY",
+                       "SMPREMQ", "PREMD", "PREMM", "PREML","EBSARP",
+                        "SMBUSB","BUSB", "BUSA", "EBSARB","BUSINESS" }
         );
         public int Compare(FlightProductBaseClass p1, FlightProductBaseClass p2)
         {
